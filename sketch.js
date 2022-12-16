820c8a312f08fb0f1a3218840edad66f9dc085aa
Sasha Solovyeva <ssolovyeva@npr.org>
2022-12-15 22:32:07 -0500

    "three tiers instead of one"

--- sketch.js
+++ sketch.js
@@ -1,14 +1,44 @@
+// ALL VARIABLES
+
 let gamePlaying;
 
 let blockSize = 70;
-let allBlocks = [];
+
+// images
 let imageUrlArray = ['assets/n.png', 'assets/p.png', 'assets/r.png'];
 let imageVarArray = [];
+let pit, heart;
+
+let font;
+let fontSize = 20;
+let successReport = '';
+
 let score;
 let lives;
 
-let font = 'Helvetica';
-let fontSize = 20;
+let currentBlock, firstBlock;
+
+// Angle and angular velocity, accleration
+let r;
+let theta;
+let theta_vel;
+let direction;
+let x;
+let y;
+
+// states
+let swingState = true;
+let dropState = false;
+let shiftBlocks = false;
+let initNewBlockState = false;
+let shiftingBool = false;
+
+// 2D array for three towers
+let allBlocks = [[], [], []]
+
+
+// BLOCK CLASSES
+//___________________________________________________________________________________________________
 
 class initBlock {
   constructor (x, y) {
@@ -23,16 +53,27 @@ class initBlock {
   }
 }
 
+function getNum () {
+  let allBlocksNums = [allBlocks[0].length, allBlocks[1].length, allBlocks[2].length];
+  if(abs(allBlocksNums[0] - allBlocksNums[1]) > 2 
+  || abs(allBlocksNums[1] - allBlocksNums[2]) > 2) {
+    return allBlocksNums.indexOf(Math.min(...allBlocksNums));
+  } else {
+    return parseInt(random(3))
+  }
+}
+
 class Block {
-  constructor (theta, theta_vel, direction, radius, x, y) {
+  constructor (theta, theta_vel, direction, radius, x, y, num) {
     this.theta = theta
     this.theta_vel = theta_vel
     this.direction = direction
     this.radius = radius
     this.x = x
     this.y = y
-    this.fallSpeed = 5;
-    this.img = random(imageVarArray);
+    this.fallSpeed = 15;
+    this.towerNum = num;
+    this.img = imageVarArray[this.towerNum];
     this.angle = 0;
   }
 
@@ -65,23 +106,25 @@ class Block {
 
   drop () {
     this.y += this.fallSpeed;
+
+    let determineRightTower = allBlocks[this.towerNum][allBlocks[this.towerNum].length - 2];
     
-    if(abs(allBlocks[allBlocks.length - 2].y - this.y) <= blockSize) {
+    if(abs(determineRightTower.y - this.y) <= blockSize) {
 
       // if placed successfully
-      if(abs(this.x - allBlocks[allBlocks.length - 2].x) < blockSize/2) {
+      if(abs(this.x - determineRightTower.x) < blockSize/2) {
         this.fallSpeed = 0;
         score += 50;
-      dropState = false;
+        dropState = false;
         initNewBlockState = true;
       }
 
       // if came close but missed
-      if(abs(this.x - allBlocks[allBlocks.length - 2].x) >= blockSize/2 
-      && abs(this.x - allBlocks[allBlocks.length - 2].x) <= blockSize) {
+      if(abs(this.x - determineRightTower.x) >= blockSize/2 
+      && abs(this.x - determineRightTower.x) <= blockSize) {
 
         // if fell to the right
-        if(this.x - allBlocks[allBlocks.length - 2].x > 0) {
+        if(this.x - determineRightTower.x > 0) {
           this.x += 5;
         } else {
           this.x -= 5;
@@ -90,7 +133,7 @@ class Block {
         if(this.y > height + 100) {
           dropState = false;
           initNewBlockState = true;
-          allBlocks.pop()
+          allBlocks[this.towerNum].pop()
         }
       }
     } 
@@ -102,36 +145,26 @@ class Block {
     if(this.y > height + 100) {
       // lose a life
       lives--;
+      // let them know that they missed
+      successReport = 'You dropped the block or chose a wrong tower. Try again!'
       
       dropState = false;
       initNewBlockState = true;
-      allBlocks.pop()
+      allBlocks[this.towerNum].pop()
     }
   }
 }
 
+
+// MAIN FUNCTIONS
 //___________________________________________________________________________________________________
 
-let r;
-let currentBlock, firstBlock;
-// Angle and angular velocity, accleration
-let theta;
-let theta_vel;
-let direction;
-let x;
-let y;
-
-let swingState = true;
-let dropState = false;
-let initNewBlockState = false;
-
-// function preload() {
-//   font = loadFont('assets/fonts/PressStart.ttf');
-// }
-
-let incrChange = 0;
 
 function preload() {
+  font = loadFont('assets/PressStart2P-Regular.ttf');
+  pit = loadImage('assets/pit-pixels.jpg');
+  heart = loadImage('assets/heart.png');
+
   for(let i = 0; i < imageUrlArray.length; i++) {
     imageVarArray[i] = loadImage(imageUrlArray[i]);
   }
@@ -145,13 +178,13 @@ function setup() {
   // font setup
   textFont(font);
   textSize(fontSize);
-  textAlign(CENTER, CENTER)
+  textAlign(CENTER)
+  textWrap(WORD);
 
   gamePlaying = true;
 
   score = 0;
   lives = 3;
-  allBlocks = [];
 
   // Initialize all values
   r = height * 0.25;
@@ -161,61 +194,81 @@ function setup() {
   x = r * cos(theta);
   y = r * sin(theta);
 
-  firstBlock = new initBlock(0, height - blockSize/2);
-  allBlocks.push(firstBlock);
+  for (let i = 0; i < allBlocks.length; i++) {
+    firstBlock = new initBlock(-width/4 + width/4 * i, height - blockSize/2);
+    allBlocks[i].push(firstBlock);
+  }
 
-  currentBlock = new Block(theta, theta_vel, direction, r, x, y)
-  allBlocks.push(currentBlock);
+  currentBlock = new Block(theta, theta_vel, direction, r, x, y, getNum())
+  allBlocks[currentBlock.towerNum].push(currentBlock);
 }
 
+let a1, a2, a3;
+
 function draw() {
-  background(0);
+  background(40);
+  tint(255, 100);
+  image(pit, width/2, height/2, 800, 800);
+  tint(255);
   // Translate the origin point to the center of the screen
   translate(width / 2, 0);
 
   // draw scoreboard
-  textAlign(LEFT);
-  drawScore(width * 0.25);
-  drawLives(width * 0.1);
+  text(successReport, 0, 200, 400)
+  drawScore(290, 50);
+  drawLives(290, 100);
 
   if (lives <= 0) {
     gamePlaying = false;
   }
 
   if (gamePlaying) {
-    firstBlock.draw();
-
-    // start at 1 to skip the initial block
-    for (i = 1; i < allBlocks.length; i++){ 
-      allBlocks[i].draw();
-    }
-    
-    if (allBlocks.length > 1) {
-      if (swingState){
-        allBlocks[allBlocks.length - 1].swing();
-        stroke(255);
-        line(0, 0, allBlocks[allBlocks.length - 1].x, allBlocks[allBlocks.length - 1].y);
-      }
-
-      if (dropState){
-        allBlocks[allBlocks.length - 1].drop();
+    for (i = 0; i < allBlocks.length; i++){ 
+      for (let j = 0; j < allBlocks[i].length; j++){
+        allBlocks[i][j].draw();
       }
     }
+  
+    if (swingState) {
+      currentBlock.swing();
+      stroke(255);
+      line(0, 0, currentBlock.x, currentBlock.y);
+      fill(255)
+    }
 
-  if (allBlocks.length > 2) {
-    if (allBlocks[allBlocks.length - 2].y <= height/2) {
-      for(let i = 0; i < allBlocks.length - 1; i++) {
-        allBlocks[i].y += 5;
+    if (dropState) {
+      successReport = '';
+      currentBlock.drop();
+    }
+
+    a1 = allBlocks[0];
+    a2 = allBlocks[1];
+    a3 = allBlocks[2];
+
+    if (a1.length > 2  && a2.length > 2 && a3.length > 2) {
+
+      if (a1[a1.length - 2].y <= height/2
+        || a2[a2.length - 2].y <= height/2
+        || a3[a3.length - 2].y <= height/2) {
+        for(let i = 0; i < allBlocks.length; i++) {
+          for(let j = 0; j < allBlocks[i].length; j++) {
+            allBlocks[i][j].y += 1;
+          }
+        }
       }
     }
-  }
+
 
     if(initNewBlockState) {
-      currentBlock = new Block(theta, theta_vel, direction, r, x, y)
-      allBlocks.push(currentBlock);
+      currentBlock = new Block(theta, theta_vel, direction, r, x, y, getNum())
+      console.log(currentBlock.towerNum)
+      allBlocks[currentBlock.towerNum].push(currentBlock);
       initNewBlockState = false;
       swingState = true;
     }
+  } else {
+    successReport = 'Thank you for playing!\nAll hail the Pit!';
+    image(heart, 0, 400, 150, 150);
   }
 }
 
@@ -231,12 +284,12 @@ function keyPressed () {
   }
 }
 
-function drawScore(position) {
+function drawScore(xPos, yPos) {
   fill(255);
-  text("SCORE: " + score.toString(), position, height * 0.90);
+  text("SCORE: " + score.toString(), xPos, yPos);
 }
 
-function drawLives(position) {
+function drawLives(xPos, yPos) {
   fill(255);
-  text("LIVES: " + lives.toString(), position, height * 0.90);
+  text("LIVES: " + lives.toString(), xPos, yPos);
 }
